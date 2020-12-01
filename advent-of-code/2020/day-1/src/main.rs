9de6d2854dabfa54dd0cea668abe51318f3639f5
Sniadek <karolgruszka9@gmail.com>
2020-12-01 23:14:08 +0100

    aoc(2020): refactor day 1 to be less performant :D

--- advent-of-code/2020/day-1/src/main.rs
+++ advent-of-code/2020/day-1/src/main.rs
@@ -1,37 +1,46 @@
+use itertools::Itertools;
 use std::io::Result;
 
-fn part_one(numbers: &Vec<u32>) -> u32 {
-    for a in numbers {
-        for b in numbers {
-            if a + b == 2020 {
-                return a * b;
-            }
-        }
-    }
-    unreachable!()
+const SEARCHED_SUM: u32 = 2020;
+
+fn sum(items: &[&u32]) -> u32 {
+    items.iter().map(|&&x| x).sum()
 }
 
-fn part_two(numbers: &Vec<u32>) -> u32 {
-    for a in numbers {
-        for b in numbers {
-            for c in numbers {
-                if a + b + c == 2020 {
-                    return a * b * c;
-                }
-            }
-        }
-    }
-    unreachable!()
+fn solve(numbers: &Vec<u32>, entries_size: usize) -> u32 {
+    numbers
+        .iter()
+        .combinations(entries_size)
+        .find(|c| sum(&c) == SEARCHED_SUM)
+        .unwrap()
+        .into_iter()
+        .product()
+}
+
+fn get_input() -> Result<Vec<u32>> {
+    Ok(std::fs::read_to_string("input.txt")?
+        .lines()
+        .map(|num| num.parse::<u32>().expect("Input consists only of numbers"))
+        .collect::<Vec<u32>>())
 }
 
 fn main() -> Result<()> {
-    let numbers: Vec<u32> = std::fs::read_to_string("input.txt")?
-        .lines()
-        .map(|num| num.parse::<u32>().expect("Input consists only of numbers"))
-        .collect();
-
-    println!("The answer to the first part is {}", part_one(&numbers));
-    println!("The answer to the second part is {}", part_two(&numbers));
-
+    let numbers = get_input()?;
+    println!("The answer to the first part is {}", solve(&numbers, 2));
+    println!("The answer to the second part is {}", solve(&numbers, 3));
     Ok(())
+}
+
+// Making sure that I won't break anything playing with the code
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn answers() -> Result<()> {
+        let numbers = get_input()?;
+        assert_eq!(solve(&numbers, 2), 870331);
+        assert_eq!(solve(&numbers, 3), 283025088);
+        Ok(())
+    }
 }
